474f40371b42037b226f05ceccca50ac347796fa
Timo Schönbeck <jason-the-rypper@live.de>
2022-12-16 13:21:37 +0100

    Initial Commit

---
+++ swiftUItest/swiftUItestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUItestApp.swift
+//  swiftUItest
+//
+//  Created by Timo Schönbeck on 16.12.22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUItestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
