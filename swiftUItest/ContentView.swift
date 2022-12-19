1064d380845bbc2e4feb769303bfc0ba155e8ef4
Timo Schönbeck <jason-the-rypper@live.de>
2022-12-19 13:23:37 +0100

    Freitags Projekt 1

--- swiftUItest/ContentView.swift
+++ swiftUItest/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            
+            Image("iv_circle_bg")
+                .resizable()
+                .scaledToFit()
+   
+           
+            Image("iv_divider")
+                .resizable()
+            
+                .scaledToFit()
+                .position(x:200,y:537)
+         
+            
+            HStack{
+                
+                Image("iv_bruno")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:500,height:500)
+                    .position(x:210,y:300)
+                ZStack{
+                    Image("iv_text")
+                    
+                        .scaledToFit()
+                        .position(x:10,y:460)
+                    
+                    HStack{
+                        Text("Psychotherapie")
+                           
+                        
+                        
+                        
+                    }
+                    
+                }
+                
+                
+                
+                
+                
+            }
+            
         }
-        .padding()
+        
     }
 }
 
